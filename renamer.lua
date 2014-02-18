793ab0fdf67ec300dadc83c05265e9be63ffee71
Jose <josellausas@gmail.com>
2014-02-18 11:15:34 -0600

    Initial commit

---
+++ renamer.lua
@@ -0,0 +1,92 @@
+-- Created by jose llausas jose@zunware.com
+
+-- function SplitMeIntolines(str)
+-- 	local t = {}
+-- 	local function helper(line) 
+-- 		table.insert(t, line) 
+-- 		return "" 
+-- 	end
+-- 	helper((str:gsub("(.-)r?n", helper)))
+-- 	return t
+-- end
+
+function splitThis(str)
+	t={}
+	for line in str:gmatch("[^\r\n]+") do  
+		table.insert(t, line)
+	end
+	return t
+end
+
+local function mySimpleExecute(command)
+	local success = nil
+	local signal  = nil
+	local number  = nil
+
+	success, signal, number = os.execute(command)
+
+	if not success then print('Error running command') return false end
+
+	return true
+end
+
+local function myExecute(command)
+	local handle = io.popen(command)
+	local result = handle:read("*a")
+	handle:close()
+
+	if not result then 
+		print('Error with cmd ' .. command) 
+	end
+
+	return result
+
+end
+
+-- Starts here
+if #arg == 0 then
+	print('USAGE:\nrenamer.lua pathToFiles')
+end
+
+local pathToFiles = arg[1]
+
+
+local comando01 = ('cd ' .. pathToFiles)
+
+
+local function getExtension(string)
+	local full = nil
+	local extension = nil
+	return string.match(string, "(.-)([^\\/]-%.?([^%.\\/]*))$")
+	
+end
+
+if mySimpleExecute(comando01) then
+	print('Renaming files inside:')
+	print(pathToFiles .. '\n')
+
+	local filesNames = myExecute('ls ' .. pathToFiles)
+	
+	local splitNames = splitThis(filesNames)
+
+	print(#splitNames)
+
+	for i=1, #splitNames do
+		-- print(splitNames[i])
+
+		local fullPathName = pathToFiles .. splitNames[i]
+		print(fullPathName)
+
+		local newName = pathToFiles .. i
+		print(newName)
+
+		getExtension
+	end
+
+
+
+
+else
+	print('Invalid Path')
+end
+
